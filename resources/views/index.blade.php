cf820f9bdbff13c37d7730eb0caf225bf1be1b41
luyibest001 <luyibest001@gmail.com>
2021-06-12 09:47:22 +1000

    change app.js path

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="/js/app.js" defer></script>
 </head>
 <div id="app">
     <example-component></example-component>

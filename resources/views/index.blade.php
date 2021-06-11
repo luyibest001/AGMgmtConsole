405416ef5f2163aaeb6dcd999b53ebec8f5cae45
luyibest001 <luyibest001@gmail.com>
2021-06-12 09:52:19 +1000

    content security policy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <head>
     <script src="https://accentgrouptest.herokuapp.com/js/app.js"></script>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 </head>
 <div id="app">
     <example-component></example-component>

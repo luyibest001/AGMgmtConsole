430035e81a639b8cd21d8af4f22e3dfd7d7ab0b0
luyibest001 <luyibest001@gmail.com>
2021-06-12 10:05:32 +1000

    add defer for app.js script

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <script src="https://accentgrouptest.herokuapp.com/js/app.js"></script>
+    <script src="https://accentgrouptest.herokuapp.com/js/app.js" defer></script>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 </head>
 <div id="app">
